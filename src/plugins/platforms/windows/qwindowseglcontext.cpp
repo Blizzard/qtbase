279158479a9c4d4fa184e14186c6ec0adc5c2bd2
Stephen Raub <sraub@blizzard.com>
2016-03-18 15:56:58 -0700

    Always try to load same dll regardless of configuration
    We will use the appropriate dll's per configuration rather than
    renaming them. This also fixes issues with sharing dlls with other
    projects (ex: CEF)
    
    Change-Id: I733830c84b7fad6da6cdce76fa6b62395382c9f1

--- src/plugins/platforms/windows/qwindowseglcontext.cpp
+++ src/plugins/platforms/windows/qwindowseglcontext.cpp
@@ -118,11 +118,7 @@ void *QWindowsLibEGL::resolve(const char *name)
 
 bool QWindowsLibEGL::init()
 {
-#ifdef QT_DEBUG
-    const char dllName[] = "libEGLd.dll";
-#else
     const char dllName[] = "libEGL.dll";
-#endif
 
     qCDebug(lcQpaGl) << "Qt: Using EGL from" << dllName;
 
@@ -178,11 +174,7 @@ void *QWindowsLibGLESv2::resolve(const char *name)
 
 bool QWindowsLibGLESv2::init()
 {
-#ifdef QT_DEBUG
-    const char dllName[] = "libGLESv2d.dll";
-#else
     const char dllName[] = "libGLESv2.dll";
-#endif
 
     qCDebug(lcQpaGl) << "Qt: Using OpenGL ES 2.0 from" << dllName;
 #if !defined(QT_STATIC) || defined(QT_OPENGL_DYNAMIC)
